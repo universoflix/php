fa71b21cf73a4d11bd4af680da837dc69aedfce5
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-09 23:58:24 -0300

    Update and rename installphp.sh to installph.sh

--- installph.sh
+++ installph.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+cd /var/www
+sudo mkdir pagamento
+sudo chown -R root:www-data pagamento
+cd /etc/apache2/sites-available
 wget https://raw.githubusercontent.com/universoflix/php/main/pagamento.conf
 chmod 777 pagamento.conf
 sudo a2ensite pagamento.conf
