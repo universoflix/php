0500ba44fb151e51347a9222cfbd70c96b6b226a
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-10 00:23:42 -0300

    Update and rename install-ioncube.sh to install-ioncub.sh

--- install-ioncub.sh
+++ install-ioncub.sh
@@ -4,9 +4,11 @@ wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.t
 tar xfz ioncube_loaders_lin_x86-64.tar.gz
 cp /tmp/ioncube/ioncube_loader_lin_7.2.so /usr/lib/php/20170718
 cd /etc/php/7.2/apache2
+rm php.ini
 wget https://raw.githubusercontent.com/universoflix/php/main/php.ini
 chmod 777 php.ini
 cd /etc/php/7.2/cli
+rm php.ini
 wget https://raw.githubusercontent.com/universoflix/php/main/php.ini
 sudo service apache2 restart	
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
